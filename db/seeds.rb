4f2d9f45bd5513293d64c86582dace3b43caf7ee
jjcaburrasi <your.email@example.com>
2022-09-28 13:30:41 +0200

    Add seeds to generate workers and clients

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,29 @@ Client.create!(name:"client",
 
 Admin.create!(email: "a@a.com", password:"123456")
 
+# Generate a bunch of workers.
+200.times do |n|
+    name  = Faker::Name.name
+    email = "example-#{n+1}@railstutorial.org"
+    password = "password"
+    skills = ["Driving", "Tech", "Hard"]
+    Worker.create!(name:  name,
+    email: email,
+    password:              password,
+    skills: skills
+    )
+end
+
+# Generate a bunch of clients.
+40.times do |n|
+    name  = Faker::Company.name
+    email = "example-#{n+1}@railstutorial.org"
+    password = "password"
+    Client.create!(name:  name,
+    email: email,
+    password:              password,
+    )
+
+
+end
+
