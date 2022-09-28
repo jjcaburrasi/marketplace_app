42879a33e9310ce70d6afad7b757f3091d92dd6a
jjcaburrasi <your.email@example.com>
2022-09-28 17:09:05 +0200

    Add worker, client, admin header partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  get 'admins/show'
   get 'clients/show'
   root "static_pages#home"
   devise_for :admins
   devise_for :clients
   devise_for :workers
   resources :job_requests, path:"jobs"
-  resources :workers, only: :show
-  resources :clients, only: :show
+  resources :workers
+  resources :clients
 end
