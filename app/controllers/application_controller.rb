69a242e69298d2b96093cbfff4706acf7ca82a70
jjcaburrasi <your.email@example.com>
2022-09-29 19:13:17 +0200

    Some minor changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,26 +13,7 @@ class ApplicationController < ActionController::Base
             devise_parameter_sanitizer.permit(:account_update, keys: [:name, :available])
         end
 
-        # def after_sign_in_path_for(resource)
-        #    @admin
-        # end   
+     
 
-        # def after_sign_up_path_for(resource)
-        #     @admin
-        #  end  
-
-        # def logged_in_user
-        #     unless user_signed_in?
-        #         flash[:danger] = "Please log in."
-        #         redirect_to new_user_session_path
-        #     end
-        # end
-
-        
-        # def admin_user
-        #     unless current_user.admin?
-        #         flash[:danger] = "Please log in as admin user."
-        #         redirect_to new_user_session_path
-        #     end   
-        # end
+    
 end
